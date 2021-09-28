85dac24dc2bf72bdf0e767318cb7107a7a99298f
Evgeny Salnikov <evgeny@salnikoff.com>
2021-09-28 23:17:15 +0300

    init

---
+++ src/test/kotlin/com/salnikoff/democonfigserver/DemoConfigServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.salnikoff.democonfigserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoConfigServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
